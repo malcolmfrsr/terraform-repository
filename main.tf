a4766dcb9665656d7f61b6eff95a0ef3cbd6cb55
malcolmfrsr <malcolmfrsr@gmail.com>
2024-03-09 16:47:06 +0200

    First working commit.

--- main.tf
+++ main.tf
@@ -1,8 +1,174 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "mf-bucket"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+#####################################################################
+###################### bucket infrastructure ########################
+#####################################################################
+resource "aws_s3_bucket" "web-repository" {
+  bucket = "mf-webcontent"
+
 
   tags = {
-    Name        = "mf-bucket"
+    Name        = "mf-webcontent"
     Environment = "test-account"
   }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  depends_on = [aws_s3_bucket.web-repository]
+
+  bucket = aws_s3_bucket.web-repository.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::238182092824:user/terraform-user"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetBucketAcl",
+      "s3:PutBucketAcl"
+    ]
+
+    resources = [aws_s3_bucket.web-repository.arn,
+      "${aws_s3_bucket.web-repository.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "web-repository" {
+  bucket = aws_s3_bucket.web-repository.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#####################################################################
+######################### website content ###########################
+#####################################################################
+
+
+#####################################################################
+##################### cloudfront distribution #######################
+#####################################################################
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "s3_distribution_access_control"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.web-repository.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = aws_s3_bucket.web-repository.id
+  }
+
+
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.web-repository.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.web-repository.id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.web-repository.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+
 }
