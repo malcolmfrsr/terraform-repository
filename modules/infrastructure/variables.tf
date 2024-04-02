69e48ea91639dbfe27ecb9fd1aadfb95cb68cc08
malcolmfrsr <malcolmfrsr@gmail.com>
2024-04-02 17:07:24 +0200

    Moced to module.

---
+++ modules/infrastructure/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "start_page" {
+  type = string
+}
+
+variable "start_page_dir" {
+  type = string
+}
+variable "error_page" {
+  type = string
+}
+
+
